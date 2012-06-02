bbecf546cc66ce617dccc0bc86159c48512b177c
ajlopez <webmaster@ajlopez.com>
2012-06-02 16:04:41 -0300

    Refactor createRemoteClient methods

--- test/createServer.js
+++ test/createServer.js
@@ -13,30 +13,37 @@ exports['Create Server'] = function(test) {
     var server = simpleremote.createRemoteServer(obj);
     server.listen(3000);
     
-    var client = simpleremote.createRemoteClient(3000).on('remote',
+    var client = simpleremote.createRemoteClient(
         function(remote) {
             test.ok(remote);
             server.close();
             client.end();
             test.done();
         });
+        
+    client.connect(3000);
 }
 
 exports['Create and Invoke Server'] = function(test) {
     test.expect(2);
     
     var obj = {
-        method: function (x) { test.equal(x, 1); test.done(); }
+        method: function (x) { 
+            test.equal(x, 1); 
+            test.done(); 
+        }
     };
 
     var server = simpleremote.createRemoteServer(obj);
     server.listen(3001);
     
-    var client = simpleremote.createRemoteClient(3001).on('remote',
+    var client = simpleremote.createRemoteClient(
         function(remote) {
             test.ok(remote);
             remote.method(1);
             server.close();
             client.end();
         });
+        
+    client.connect(3001);
 }
