35e7b35b52093bb49b8a06f0a7a24e288b287dfe
ajlopez <webmaster@ajlopez.com>
2012-06-02 18:10:53 -0300

    remote return, remote exception, with tests; npm publish

--- test/createServer.js
+++ test/createServer.js
@@ -35,7 +35,7 @@ exports['Create and Invoke Server'] = function(test) {
     };
 
     var server = simpleremote.createRemoteServer(obj);
-    server.listen(3001);
+    server.listen(3000);
     
     var client = simpleremote.createRemoteClient(
         function(remote) {
@@ -45,5 +45,143 @@ exports['Create and Invoke Server'] = function(test) {
             client.end();
         });
         
-    client.connect(3001);
+    client.connect(3000);
+}
+
+exports['Invoke Remote Method and Get Simple Return'] = function(test) {
+    test.expect(3);
+    
+    var obj = {
+        add: function(x,y) { 
+            return x + y;
+        }
+    };
+
+    var server = simpleremote.createRemoteServer(obj);
+    server.listen(3000);
+    
+    var client = simpleremote.createRemoteClient(
+        function(remote) {
+            test.ok(remote);
+            remote.add(2, 3, function(val) {
+                test.ok(val);
+                test.equal(val, 5);
+                server.close();
+                client.end();
+                test.done();
+            });
+        });
+        
+    client.connect(3000);
+}
+
+exports['Get Simple Return without Error'] = function(test) {
+    test.expect(3);
+    
+    var obj = {
+        add: function(x,y) { 
+            return x + y;
+        }
+    };
+
+    var server = simpleremote.createRemoteServer(obj);
+    server.listen(3000);
+    
+    var client = simpleremote.createRemoteClient(
+        function(remote) {
+            test.ok(remote);
+            remote.add(2, 3, function(err, val) {
+                test.ok(val);
+                test.equal(val, 5);
+                server.close();
+                client.end();
+                test.done();
+            });
+        });
+        
+    client.connect(3000);
+}
+
+exports['Get Remote Error'] = function(test) {
+    test.expect(4);
+    
+    var obj = {
+        eval: function(text) { 
+            return eval(text);
+        }
+    };
+
+    var server = simpleremote.createRemoteServer(obj);
+    server.listen(3000);
+    
+    var client = simpleremote.createRemoteClient(
+        function(remote) {
+            test.ok(remote);
+            remote.eval("a+2", function(err, val) {
+                test.ok(err);
+                test.ok(typeof err == 'string');
+                test.ok(err.indexOf('ReferenceError') >= 0);
+                server.close();
+                client.end();
+                test.done();
+            });
+        });
+        
+    client.connect(3000);
+}
+
+exports['Remote Simple Eval'] = function(test) {
+    test.expect(3);
+    
+    var obj = {
+        eval: function(text) { 
+            return eval(text);
+        }
+    };
+
+    var server = simpleremote.createRemoteServer(obj);
+    server.listen(3000);
+    
+    var client = simpleremote.createRemoteClient(
+        function(remote) {
+            test.ok(remote);
+            remote.eval("2+3", function(val) {
+                test.ok(val);
+                test.equal(val, 5);
+                server.close();
+                client.end();
+                test.done();
+            });
+        });
+        
+    client.connect(3000);
+}
+
+
+exports['Serialize Simple Object'] = function(test) {
+    test.expect(4);
+    
+    var obj = {
+        get: function(name) { 
+            return { name: name, length: name.length };
+        }
+    };
+
+    var server = simpleremote.createRemoteServer(obj);
+    server.listen(3000);
+    
+    var client = simpleremote.createRemoteClient(
+        function(remote) {
+            test.ok(remote);
+            remote.get("foo", function(val) {
+                test.ok(val);
+                test.equal(val.name, "foo");
+                test.equal(val.length, 3);
+                server.close();
+                client.end();
+                test.done();
+            });
+        });
+        
+    client.connect(3000);
 }
